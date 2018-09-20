cdec04c6810f59afe8f8171fd2acf0c4763b9fac
wangyan6783 <wangyan6783@gmail.com>
2018-09-19 20:23:52 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,39 @@
-class CrewMember {}
+class CrewMember {
+  constructor (position, currentShip){
+    this.position = position
+    this.currentShip = currentShip
+    if (!this.currentShip) {
+      this.currentShip = 'Looking for a Rig'
+    }
+  }
+  chargePhasers(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+    if (this.position === "Gunner"){
+      this.currentShip.phasersCharge = "charged"
+    } else {
+      this.currentShip.phasersCharge = "uncharged"
+    }
+  }
+  engageWarpDrive(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+    if(this.position === "Pilot"){
+      this.currentShip.warpDrive = "engaged"
+    } else {
+      this.currentShip.warpDrive = "disengaged"
+    }
+  }
+  setsInvisibility(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+    if(this.position === "Defender"){
+      this.currentShip.cloaked = true
+    } else {
+      this.currentShip.cloaked = false
+    }
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,19 @@
-class Spaceship {}
+class Spaceship {
+  constructor (name, crew, phasers, shields, cloaked=false, warpDrive="disengaged", docked, phasersCharge="uncharged") {
+    this.name = name;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = cloaked;
+    this.warpDrive = warpDrive;
+    this.phasersCharge = phasersCharge;
+    if (crew.length > 0){
+      this.crew = crew;
+      for (let member of crew){
+        member.currentShip = this
+      }
+      this.docked = false
+    }else{
+      this.docked = true
+    }
+  }
+}
